9c972d7e6d52cf4be192aa48bd8820458290eb76
岩元 隆明 <iwamto.takaaki@gmail.com>
2022-09-04 15:38:34 +0900

    add config

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -7,14 +7,20 @@
  * 4. Copy the config object snippet, then add it here.
  */
  const config = {
-
+    apiKey: "AIzaSyCsLrBhQ_av_rext-ob38nZQhEhZASE6fE",
+    authDomain: "planning-6317a.firebaseapp.com",
+    projectId: "planning-6317a",
+    storageBucket: "planning-6317a.appspot.com",
+    messagingSenderId: "883064645820",
+    appId: "1:883064645820:web:5bf2507629120159a548b9",
+    measurementId: "G-6YDFBF3WXE"
 };
   
 export function getFirebaseConfig() {
-if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
-} else {
-    return config;
-}
+    if (!config || !config.apiKey) {
+        throw new Error('No Firebase configuration object provided.' + '\n' +
+        'Add your web app\'s configuration object to firebase-config.js');
+    } else {
+        return config;
+    }
 }
