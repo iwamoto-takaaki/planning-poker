d82df47fb32781329db6f681f2470f8d60d62c45
岩元 隆明 <iwamto.takaaki@gmail.com>
2022-09-04 15:27:30 +0900

    add firebase-config

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import { initializeApp } from 'firebase/app';
+import { getFirebaseConfig } from './firebase-config.ts';
+
 createApp(App).mount('#app')
+
+const firebaseApp = initializeApp(getFirebaseConfig());
