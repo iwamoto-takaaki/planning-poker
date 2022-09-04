6703f8e3710a50d0924bcff34c59658bfc35835e
岩元 隆明 <iwamto.takaaki@gmail.com>
2022-09-04 17:43:27 +0900

    add auth

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
-<script setup>
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+import { initializeApp } from 'firebase/app';
+import { getFirebaseConfig } from './firebase-config';
+import { getPerformance } from 'firebase/performance';
+
+import {
+  User,
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import { makeBlock } from '@vue/compiler-core';
+
+const firebaseApp = initializeApp(getFirebaseConfig());
+getPerformance();
+
+interface State {
+  user: User | null,
+}
+
+const state = reactive<State>({
+  user: null
+});
+
+const signInUser = async () => await signInWithPopup(getAuth(), new GoogleAuthProvider())
+const signOutUser = () => signOut(getAuth()) 
+
+onAuthStateChanged(getAuth(), (user) => state.user = user);
 </script>
 
 <template>
   <div>
+    <div id="user-container">
+      <div v-if="state.user">
+        <div>{{ state.user.displayName }}</div>
+        <button @click="signOutUser"> Sign Out </button>
+      </div>
+      <div v-if="!state.user">
+        <button @click="signInUser"> Sign In </button>
+      </div>
+    </div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
