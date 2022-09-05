ce022bdb7fe8d95cd408d39b81c3e6b08b68b21a
岩元 隆明 <iwamto.takaaki@gmail.com>
2022-09-06 05:48:07 +0900

    add router

--- src/App.vue
+++ src/App.vue
@@ -15,19 +15,14 @@ const counter = getCounter()
 </script>
 
 <template>  
-  <div>
-    <Header
-      :user="currentUser.user.value"
-      @sign-in-user="currentUser.signInUser"
-      @sign-out-user="currentUser.signOutUser"
-    />
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <Header
+    :user="currentUser.user.value"
+    @sign-in-user="currentUser.signInUser"
+    @sign-out-user="currentUser.signOutUser"
+  />
+
+  <router-view></router-view>
+
   <HelloWorld
     msg="Vite + Vue"
     :count="counter.count.value"
@@ -35,16 +30,3 @@ const counter = getCounter()
   />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/router.js
@@ -0,0 +1,9 @@
+import Home from './components/pages/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+export default new createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'home', component: Home }
+  ]
+})
