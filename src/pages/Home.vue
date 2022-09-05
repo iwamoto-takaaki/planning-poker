e739bca3e0e8ec969a1964d005f90fb8447df31a
岩元 隆明 <iwamto.takaaki@gmail.com>
2022-09-06 06:29:01 +0900

    add Room.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,13 +6,16 @@
 </script>
 
 <template>
-      <div>
+  <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+  </div>
+  <div>
+    <router-link to="/room/1234">新規ルーム作成</router-link>
   </div>
 
   <HelloWorld
--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
-import Home from './pages/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import Room from './pages/Room.vue'
+import Home from './pages/Home.vue'
 
 export default new createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/room/:id', component: Room },
     { path: '/', name: 'home', component: Home }
   ]
 })
